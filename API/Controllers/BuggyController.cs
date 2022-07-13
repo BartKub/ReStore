463fa8459a2fe5c26ddc1e16367ecbcef2de9672
Bartłomiej Kubica <bartlomiej.kubica@ext.nordiska.com>
2022-07-13 15:53:32 +0200

    Controllers andexceptoin middleware

---
+++ API/Controllers/BuggyController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class BuggyController: BaseApiController
+    {
+        [HttpGet("not-found")]
+        public ActionResult GetNotFound()
+        {
+            return NotFound();
+        }
+
+        [HttpGet("bad-request")]
+        public ActionResult GetBadRequest()
+        {
+            return BadRequest(new ProblemDetails{Title = "this is a bad request"});
+        }
+
+        [HttpGet("unauthorized")]
+        public ActionResult GetUnauthorized()
+        {
+            return Unauthorized();
+        }
+
+        [HttpGet("validation-error")]
+        public ActionResult GetValidationError()
+        {
+            ModelState.AddModelError("Proplem1", "This is first error");
+            ModelState.AddModelError("Proplem2", "This is second error");
+
+            return ValidationProblem();
+        }
+
+        [HttpGet("server-error")]
+        public ActionResult GetServerError()
+        {
+            throw new Exception("Server error");
+        }
+    }
+}
--- client/src/features/about/About.tsx
+++ client/src/features/about/About.tsx
@@ -1,5 +1,56 @@
-import { Typography } from '@mui/material';
+import { Button, ButtonGroup, Container, Typography } from '@mui/material';
+import agent from '../../app/api/agent';
 
 export default function AboutPage() {
-  return <Typography variant="h2">About page</Typography>;
+  return (
+    <Container>
+      <Typography gutterBottom variant="h2">
+        Errors for testing purposes
+      </Typography>
+      <ButtonGroup fullWidth>
+        <Button
+          variant="contained"
+          onClick={() =>
+            agent.TestErrors.get400Error().catch((err) => console.log(err))
+          }
+        >
+          Test 400
+        </Button>
+        <Button
+          variant="contained"
+          onClick={() =>
+            agent.TestErrors.get401Error().catch((err) => console.log(err))
+          }
+        >
+          Test 401
+        </Button>
+        <Button
+          variant="contained"
+          onClick={() =>
+            agent.TestErrors.get404Error().catch((err) => console.log(err))
+          }
+        >
+          Test 404
+        </Button>
+        <Button
+          variant="contained"
+          onClick={() =>
+            agent.TestErrors.get500Error().catch((err) => console.log(err))
+          }
+        >
+          Test 500
+        </Button>
+        <Button
+          variant="contained"
+          onClick={() =>
+            agent.TestErrors.getValidationError().catch((err) =>
+              console.log(err)
+            )
+          }
+        >
+          Test Validation Errors
+        </Button>
+      </ButtonGroup>
+    </Container>
+  );
 }
