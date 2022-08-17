64be74f1fb9c3eb22d755c63f47dae2c636accb2
Bartłomiej Kubica <bartlomiej.kubica@ext.nordiska.com>
2022-08-17 11:03:58 +0200

    rename prop

--- API/Dtos/UserDto.cs
+++ API/Dtos/UserDto.cs
@@ -4,5 +4,5 @@ public class UserDto
 {
     public string Email { get; set; }
     public string Token { get; set; }
-    public BasketDto Baske { get; set; }
+    public BasketDto Basket { get; set; }
 }
